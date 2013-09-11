0e0a3ec05b369f02bfa5c384870329ea14ff23e7
Shuma.F <shuma@ShumaF-no-MacBook-Air.local>
2013-09-12 00:16:44 +0900

    JScrollPaneを追加した．

--- src/main/java/jp/shuma/mvnIDE/view/View.java
+++ src/main/java/jp/shuma/mvnIDE/view/View.java
@@ -2,7 +2,11 @@ package jp.shuma.mvnIDE.view;
 
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
+import javax.swing.JTree;
 import java.awt.Dimension;
+import javax.swing.BoxLayout;
+import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
 
 import jp.shuma.mvnIDE.model.Model;
 import jp.shuma.mvnIDE.controller.Controller;
@@ -23,9 +27,19 @@ public class View extends JPanel{
      */
     public View() {
         super.setPreferredSize(new Dimension(550, 600));
+        JTree tree = new JTree();
         JTextPane textpane = new JTextPane();
+        super.setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
         textpane.setPreferredSize(new Dimension(550, 600));
         textpane.setVisible(true);
+//        tree.setPreferredSize(new Dimension(200, 600));
+        
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.getViewport().setView(tree);
+        scrollPane.setPreferredSize(new Dimension(200, 600));
+        
+        super.add(scrollPane);
+        super.add(new JSeparator(JSeparator.VERTICAL));
         super.add(textpane);
     }
     /**
